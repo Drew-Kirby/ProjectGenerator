d9d2822b30597fbd3e7d8cc00089e2793fa8e1d5
Drew-Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-05-13 10:03:42 -0400

    Added aditional info for each project, removed unnecessary JSON file.

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const button = document.getElementById('generatebtn');
 const projName = document.getElementById('projectlbl');
 const desc = document.getElementById('projectdesc');
+const skilllvl = document.getElementById('difficulty');
+const estimatedTime = document.getElementById('estimatedTime');
 
 let currentProjectIndex = 0;
 
 button.addEventListener('click', () =>{    
     let currentProject = projectsData[currentProjectIndex]
+    
     projName.textContent = currentProject.title
     desc.textContent = currentProject.description
+    skilllvl.textContent = "Difficulty: " + currentProject.difficulty
+    estimatedTime.textContent = "Estimated time: " + currentProject.estimated_hours + " hours"
 
     currentProjectIndex++
 
     if(currentProjectIndex == 30)
         currentProjectIndex = 0;
 
-    //console.log(projName)
+    //console.log(projName) here for debugging
     //console.log(desc)
 })
