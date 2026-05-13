8db7eec686aedf057f400964546584cf94e2f3c3
Drew-Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-05-13 09:19:31 -0400

    Fixed iteration logic, added styles.css

--- ProjectGenerator/script.js
+++ ProjectGenerator/script.js
@@ -1,27 +1,19 @@
 const button = document.getElementById('generatebtn');
-const name = document.getElementById('projectlbl');
+const projName = document.getElementById('projectlbl');
 const desc = document.getElementById('projectdesc');
 
 let currentProjectIndex = 0;
 
-button.addEventListener('click', () =>{
-    fetch('projects.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Response not ok' + response.statusText);
-            }
-            return response.json();
-        })
-        .then(projectsArray => {
-            const selectedProject = projectsArray[currentProjectIndex];
+button.addEventListener('click', () =>{    
+    let currentProject = projectsData[currentProjectIndex]
+    projName.textContent = currentProject.title
+    desc.textContent = currentProject.description
 
-            name.textContent = selectedProject.title;
-            desc.textContent = selectedProject.description;
+    currentProjectIndex++
 
-            currentProjectIndex = (currentProjectIndex + 1) % projectsArray.length;
-        })
-        .catch(error => {
-            console.error('Error loading JSON file: ', error);
-            desc.textContent = 'Could not load proejct data.';
-        })
+    if(currentProjectIndex == 30)
+        currentProjectIndex = 0;
+
+    //console.log(projName)
+    //console.log(desc)
 })
