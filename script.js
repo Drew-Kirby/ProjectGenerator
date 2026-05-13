30f3b72fe45ea0daad0539fa1a7300d8dd0e9df2
Drew-Kirby <159677238+Drew-Kirby@users.noreply.github.com>
2026-05-13 13:12:34 -0400

    Added a reset button and footer.

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-const button = document.getElementById('generatebtn');
+const genbtn = document.getElementById('generatebtn');
+const resetbtn = document.getElementById('resetbtn');
 const projName = document.getElementById('projectlbl');
 const desc = document.getElementById('projectdesc');
 const skilllvl = document.getElementById('difficulty');
 const estimatedTime = document.getElementById('estimatedTime');
+let currentProjectIndex;
 
-let currentProjectIndex = 0;
+let savedValue = localStorage.getItem("projectIndex");
 
-button.addEventListener('click', () =>{    
-    let currentProject = projectsData[currentProjectIndex]
-    
-    projName.textContent = currentProject.title
-    desc.textContent = currentProject.description
-    skilllvl.textContent = "Difficulty: " + currentProject.difficulty
-    estimatedTime.textContent = "Estimated time: " + currentProject.estimated_hours + " hours"
+if (savedValue === null) {
+    currentProjectIndex = 0;
+} else {
+    currentProjectIndex = parseInt(savedValue);
+    updateUI();
+}
 
+genbtn.addEventListener('click', () =>{    
     currentProjectIndex++
 
     if(currentProjectIndex == 30)
         currentProjectIndex = 0;
 
+    updateUI();
+
+    localStorage.setItem("projectIndex", currentProjectIndex);
+
     //console.log(projName) here for debugging
     //console.log(desc)
 })
+
+resetbtn.addEventListener('click', () =>{
+    localStorage.removeItem("projectIndex");
+    currentProjectIndex = 0;
+    projName.textContent = "Click the button below to generate an idea for a programming project!"
+    desc.textContent = ""
+    skilllvl.textContent = ""
+    estimatedTime.textContent = ""
+})
+
+function updateUI() {
+    let currentProject = projectsData[currentProjectIndex]
+    projName.textContent = currentProject.title
+    desc.textContent = currentProject.description
+    skilllvl.textContent = "Difficulty: " + currentProject.difficulty
+    estimatedTime.textContent = "Estimated time: " + currentProject.estimated_hours + " hours"
+}
